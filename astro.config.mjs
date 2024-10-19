26144c255e7d54547d5c8b0103ea96a8fb47415f
Alessandro Casazza <imasterale@gmail.com>
2024-10-19 18:03:59 +0200

    Update vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   // site: "https://twcrypto.watch/",
   site: "https://twcrypto-watch.vercel.app/",
   integrations: [tailwind(), icon()],
-  output: "hybrid",
+  output: "static",
   adapter: vercel({
     imageService: true,
     webAnalytics: {
