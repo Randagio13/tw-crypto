024cbe20ff3f0b74606c75686ceffd34b036c31a
Alessandro Casazza <imasterale@gmail.com>
2024-10-19 18:25:23 +0200

    Update vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
   // site: "https://twcrypto.watch/",
-  site: "https://twcrypto-watch.vercel.app/",
+  site: "https://twcrypto.vercel.app/",
   integrations: [tailwind(), icon()],
   output: "static",
   adapter: vercel({
